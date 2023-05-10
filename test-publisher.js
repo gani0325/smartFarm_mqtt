91356e2d17e8efc4d0209320e89c64e8d7b3e925
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-10 13:31:57 +0900

    ➕[Feat] db 들어왔는지 test publish

--- test-publisher.js
+++ test-publisher.js
@@ -1,6 +1,6 @@
-import mqtt from 'mqtt'
-import dotenv from 'dotenv';
-dotenv.config()
+import mqtt from "mqtt";
+import dotenv from "dotenv";
+dotenv.config();
 
 const mqttOptions = {
   host: process.env.MQTT_BROKER_HOST,
@@ -9,17 +9,19 @@ const mqttOptions = {
 
 const client = mqtt.connect(mqttOptions);
 
-client.on('connect', (connack) => {
-  console.log('## test publisher connected')
+client.on("connect", (connack) => {
+  console.log("## test publisher connected");
 
-  setInterval(()=>{
-    console.log("## published")
-    client.publish('dt/test', JSON.stringify({
-      device_id: 1,
-      temperature: 20, 
-      humidity: 30,
-      timestamp: Date.now(),
-    }));
-  }, 1000)
+  setInterval(() => {
+    console.log("## published");
+    client.publish(
+      "dt/test-01",
+      JSON.stringify({
+        device_id: 1,
+        temperature: 20,
+        humid: 30,
+        soil: 35,
+      })
+    );
+  }, 1000);
 });
-
