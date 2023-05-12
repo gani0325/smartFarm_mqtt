0d8fc5e3493e0e0e18953ea12b3313167b59870f
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-12 16:53:56 +0900

    📁[Rename] 폴더명 변경 및 react 다시 설치

--- simple-iot-client/src/config.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "http://127.0.0.1:8080/api";
-
-export const API = {
-  GET_REALTIME_DATA: `${BASE_URL}/data/realtime`,
-  GET_HISTORY_DATA: `${BASE_URL}/data/devices`,
-  GET_DEVICES: `${BASE_URL}/devices`,
-  POST_CMD: `${BASE_URL}//cmd/devices`,
-};
