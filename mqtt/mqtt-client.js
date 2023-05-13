1b108a58dc211c513237a0db0fdc200c7ad98342
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-13 18:21:12 +0900

    ➕[Feat] docker로 mysql 연결하기

--- mqtt/mqtt-client.js
+++ mqtt/mqtt-client.js
@@ -1,41 +1,41 @@
-import mqtt from 'mqtt'
+import mqtt from "mqtt";
 
 class MqttClient {
   #options;
   #client;
   #topics;
 
-  constructor(options, topics){
+  constructor(options, topics) {
     this.#options = options;
     this.#topics = topics;
   }
 
-  connect(){
+  connect() {
     const self = this;
+
     self.#client = mqtt.connect(self.#options);
-    
-    self.#client.on('connect', () => {
-      console.log('## connected');
+    self.#client.on("connect", () => {
+      console.log("## connected");
 
       // 구독 코드
       self.#client.subscribe(self.#topics, (error) => {
         if (!error) {
-          console.log("구독 성공");  
+          console.log("구독 성공");
         } else {
-          console.log("구독 실패");  
+          console.log("구독 실패");
         }
       });
     });
 
     // 에러 이벤트 코드
-    self.#client.on('error', (error) => {
+    self.#client.on("error", (error) => {
       console.log(error);
     });
   }
 
   // 메시지 이벤트 콜백 설정 메소드
-  setMessageCallback(cb){
-    this.#client.on('message', cb);
+  setMessageCallback(cb) {
+    this.#client.on("message", cb);
   }
 }
 
