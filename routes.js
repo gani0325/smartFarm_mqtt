248b6189cc656573bf65896b4c42a83e72390b60
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-15 01:19:21 +0900

    ➕[Feat] api 설계

--- routes.js
+++ routes.js
@@ -3,22 +3,42 @@ import express from "express";
 const router = express.Router();
 
 const init = (db, mqttClient) => {
-  router.get("/data/realtime", async (req, res) => {
-    // 실시간 데이터 조회
+  // 실시간 데이터 조회
+  router.get("/realtime", async (req, res) => {
     res.send(await db.getLatestData());
   });
 
-  router.get("/data/devices/:device_id", async (req, res) => {
+  // 디바이스 리스트 조회
+  router.get("/devices", async (req, res) => {
+    res.send(await db.getDevices());
+  });
+
+  // 디바이스 정보 조회
+  router.get("/devices/:device_id", async (req, res) => {
+    const { device_id } = req.params;
+    console.log(device_id);
+
+    if (!device_id || device_id === "") {
+      res.status(400).send({ error: "device_id empty error" });
+    }
+
+    const devices = await db.getOneDevice(device_id);
+    res.send(await devices);
+  });
+
+  // 히스토리 데이터 조회
+  router.get("/devices/history/:device_id", async (req, res) => {
     const { device_id } = req.params;
     const { start, end } = req.query;
 
     if (!device_id || device_id === "") {
       res.status(400).send({ error: "device_id error" });
     }
-    // 히스토리 데이터 조회
+
     res.send(await db.getData(device_id, start, end));
   });
 
+  // 디바이스 명령 run & stop
   router.post("/cmd/devices/:device_id", async (req, res) => {
     const { device_id } = req.params;
     const { command } = req.body;
@@ -36,8 +56,8 @@ const init = (db, mqttClient) => {
     const device = await db.getOneDevice(device_id);
 
     // 명령 메시지 발행
-    await mqttClient.sendCommand(`cmd/${device.serial_num}/pump`, {
-      serial_num: device.serial_num,
+    await mqttClient.sendCommand(`cmd/${device_id}/motor/run`, {
+      // serial_num: device.serial_num,
       command,
     });
 
