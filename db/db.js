94c34783a4a44b9da65a264a5a705ed123a77a51
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-14 22:22:01 +0900

    ➕[Feat] 실시간 데이터, 디바이스 리스트, 디바이스 정보 조회하기

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2";
 
 class DB {
-  constructor({ host, post, user, password, database }) {
+  constructor({ host, user, post, password, database }) {
     this.pool = mysql.createPool({
       host,
-      post,
       user,
+      post,
       password,
       database,
       waitForConnections: true,
@@ -14,10 +14,11 @@ class DB {
       idleTimeout: 60000,
       queueLimit: 0,
     });
+    console.log(host, user, post, password, database);
     this.promisePool = this.pool.promise();
   }
 
-  // data insert
+  // 1) 식물 데이터 넣기
   async insertData({
     device_id,
     plantName,
@@ -49,6 +50,27 @@ class DB {
       humid,
       soil,
     ]);
+    return rows;
+  }
+
+  async getLatestData() {
+    // 2) 실시간 데이터 조회
+    const sql = `SELECT * FROM device_data WHERE idx IN(SELECT MAX(idx) idx FROM device_data);`;
+    const [rows] = await this.promisePool.query(sql);
+    return rows;
+  }
+
+  async getDevices() {
+    // 3) 디바이스 리스트 조회
+    const sql = `SELECT device_id FROM device_data;`;
+    const [rows] = await this.promisePool.query(sql);
+    return rows;
+  }
+
+  async getOneDevice(device_id) {
+    // 4) 디바이스 정보 조회
+    const sql = `SELECT * FROM device_data WHERE device_id=?;`;
+    const [rows] = await this.promisePool.query(sql, [device_id]);
     return rows;
   }
 }
