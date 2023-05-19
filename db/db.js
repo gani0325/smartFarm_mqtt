45644a48ae4aca1ed732667f1927f7e620c6d215
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-19 18:50:12 +0900

    🚧[Refactor] front와 연동 API 및 데이터 타입 조율

--- db/db.js
+++ db/db.js
@@ -17,7 +17,7 @@ class DB {
     console.log(host, user, post, password, database);
     this.promisePool = this.pool.promise();
   }
-
+  //front===========================
   // 1) 식물 데이터 넣기
   async insertData({
     plantName,
@@ -32,53 +32,58 @@ class DB {
     humid,
     soil,
   }) {
-    // device_id 를 realtime table 에서 가져옴
-    const getDeviceSQL = `SELECT device_id FROM realtime ORDER BY created_at desc LIMIT 1;`;
-    const [deviceId] = await this.promisePool.query(getDeviceSQL);
-    const device_id = deviceId[0];
+    // deviceId 를 realtime table 에서 가져옴
+    const getDeviceSQL = `SELECT deviceId FROM device ORDER BY id desc LIMIT 1;`;
+    const [device_id] = await this.promisePool.query(getDeviceSQL);
+    const deviceId = device_id[0];
 
-    if (!device_id || device_id === "") {
-      return Error("디바이스를 연결해주세요");
-    } else {
-      const sql = `INSERT INTO device_data (device_id, plantName, plantNickName, created_at, memo, potDiameter, potHeight, plantDiameter, plantHeight, region, temperature, humid, soil) values (?,?,?,?,?,?,?,?,?,?,?,?,?);`;
-      const [rows] = await this.promisePool.query(sql, [
-        device_id,
-        plantName,
-        plantNickName,
-        DEFAULT,
-        memo,
-        potDiameter,
-        potHeight,
-        plantDiameter,
-        plantHeight,
-        region,
-        temperature,
-        humid,
-        soil,
-      ]);
-      return rows;
-    }
+    const sql = `INSERT INTO device_data (deviceId, plantName, plantNickName, createdAt, memo, potDiameter, potHeight, plantDiameter, plantHeight, region, temperature, humid, soil) values (?,?,?,?,?,?,?,?,?,?,?,?,?);`;
+    const [rows] = await this.promisePool.query(sql, [
+      deviceId,
+      plantName,
+      plantNickName,
+      DEFAULT,
+      memo,
+      potDiameter,
+      potHeight,
+      plantDiameter,
+      plantHeight,
+      region,
+      temperature,
+      humid,
+      soil,
+    ]);
+    return rows;
   }
+
+  // 디바이스 아이디 받아오기
   async getDeviceId() {
-    const getDeviceSQL = `SELECT device_id FROM realtime ORDER BY created_at desc LIMIT 1;`;
-    const [deviceId] = await this.promisePool.query(getDeviceSQL);
-    const device_id = deviceId[0];
-    return device_id;
+    // deviceId 를 device table 에서 가져옴
+    const getDeviceSQL = `SELECT deviceId FROM device ORDER BY id desc LIMIT 1;`;
+    const [device_id] = await this.promisePool.query(getDeviceSQL);
+    const deviceId = device_id[0];
+    return deviceId;
   }
 
-  // 2) 모든 디바이스 리스트 조회
+  // 2) 모든 디바이스 리스트 조회 (애칭으로 검색하면 디바이스 아이디, 애칭, 식물명 나오게)
   async getDevices() {
-    const sql = `SELECT plantName, plantNickName FROM device_data;`;
+    console.log("Dhlsdfjhadfljsa;dfjasd;flasfd");
+
+    const sql = `SELECT deviceId, plantName, plantNickName FROM device_data;`;
     const [rows] = await this.promisePool.query(sql);
+
+    console.log(rows);
+
     return rows;
   }
 
   // 3) 한 개 디바이스 정보 조회
-  async getOneDevice(device_id) {
-    const sql = `SELECT * FROM device_data WHERE device_id=?;`;
-    const [rows] = await this.promisePool.query(sql, [device_id]);
+  async getOneDevice(deviceId) {
+    const sql = `select * from device_data where deviceId = ?;`;
+    const [rows] = await this.promisePool.query(sql, [deviceId]);
     return rows;
   }
+
   // 4) 한 개 디바이스 정보 수정
   async putOneDevice(
     plantName,
@@ -92,9 +97,9 @@ class DB {
     temperature,
     humid,
     soil,
-    device_id
+    deviceId
   ) {
-    const sql = `UPDATE device_data set plantName=?, plantNickName=?, memo=?, potDiameter=?, potHeight=?, plantDiameter=?, plantHeight=?, region=?, temperature=?, humid=? WHERE device_id=?;`;
+    const sql = `UPDATE device_data set plantName=?, plantNickName=?, memo=?, potDiameter=?, potHeight=?, plantDiameter=?, plantHeight=?, region=?, temperature=?, humid=? WHERE deviceId=?;`;
     const [rows] = await this.promisePool.query(sql, [
       plantName,
       plantNickName,
@@ -107,14 +112,70 @@ class DB {
       temperature,
       humid,
       soil,
-      device_id,
+      deviceId,
     ]);
     return rows;
   }
+
   // 5) 한 개 디바이스 정보 삭제
-  async deleteOneDevice(device_id) {
-    const sql = `DELETE device_data WHERE device_id=?;`;
-    const [rows] = await this.promisePool.query(sql, [device_id]);
+  async deleteOneDevice(deviceId) {
+    const sql = `DELETE device_data WHERE deviceId=?;`;
+    const [rows] = await this.promisePool.query(sql, [deviceId]);
+    return rows;
+  }
+
+  //device===========================
+  async insertDevice(deviceId) {
+    const checkSQL = `select * from device where deviceId=${deviceId};`;
+    const [checkRow] = await this.promisePool(checkSQL);
+
+    if (checkRow == null) {
+      const sql = `insert into device(deviceId) values(${deviceId});`;
+      const [rows] = await this.promisePool.query(sql);
+      return rows;
+    } else {
+      return "already exist";
+    }
+  }
+
+  //realtime===========================
+  // 실시간 데이터 저장
+  async insertRealtimeData({ deviceId, temperature, humid, soil, createdAt }) {
+    const getNameSQL = `select plantNickName from device_data where deviceId = ?;`;
+    const [result] = await this.promisePool.query(getNameSQL, deviceId);
+    const plantName = result[0];
+    // const plant_name = "aaa";
+
+    const sql = `insert into realtime values(?, ?, ?, ?, ?, ?);`;
+    const [rows] = await this.promisePool.query(sql, [
+      deviceId,
+      plantName,
+      temperature,
+      humid,
+      soil,
+      createdAt,
+    ]);
+    return rows;
+  }
+
+  // 차트 만들기 위한 데이터 로그 불러오기 -> 가장 최근 12개
+  async getRealtimes(deviceId) {
+    const sql = `select * from realtime where deviceId=${deviceId} order by createdAt desc limit 12;`;
+    const [rows] = await this.promisePool.query(sql);
+    return rows;
+  }
+
+  async getRealtimeData(deviceId) {
+    const sql = `select * from realtime where deviceId = ? order by createdAt desc limit 12;`;
+    const [rows] = await this.promisePool.query(sql, deviceId);
+    return rows;
+  }
+
+  // 디바이스별 실시간 데이터 조회
+  async getLastestRealtimeData(deviceId) {
+    const sql = `select * from realtime where deviceId = ? order by createdAt desc limit 1;`;
+    const [rows] = await this.promisePool.query(sql, [deviceId]);
+    console.log(rows);
     return rows;
   }
 }
--- index.js
+++ index.js
@@ -1,67 +1,72 @@
 import dotenv from "dotenv";
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import MqttClient from "./mqtt/mqtt-client.js";
 import DB from "./db/db.js";
 import api from "./routes.js";
-import cors from "cors";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const PORT = 3000;
-const TOPIC_TYPE_INDEX = 0;
 
 dotenv.config();
 const app = express();
-app.use(cors());
+const PORT = 3000;
+const TOPIC_TYPE_INDEX = 0;
+const SUB_TOPIC_INDEX = 1;
+let topicDeviceId;
 
+console.log("DBConnection...");
 const db = new DB({
-  // db 연결 정보 추가
-  host: "52.79.210.192",
-  user: "root",
-  post: 3306,
-  password: "Admin1234~",
-  database: "smartFarm",
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
+console.log("DBConnection... 'OK");
 
+console.log("mqttConnection...");
 const mqttOptions = {
-  // mqtt option 설정
-  host: "52.79.210.192",
-  port: 1883,
+  host: process.env.MQTT_BROKER_HOST,
+  port: process.env.MQTT_BROKER_PORT,
+  username: process.env.MQTT_BROKER_USERNAME,
+  password: process.env.MQTT_BROKER_PASSWORD,
 };
 
-const mqttClient = new MqttClient(mqttOptions, ["dt/#"]); // 구독할 토픽 추가
+const mqttClient = new MqttClient(mqttOptions, ["data/#", "registration/#"]); // 구독할 토픽 추가
 mqttClient.connect();
 
 mqttClient.setMessageCallback(async (topic, message) => {
   // 메시지 이벤트 콜백 설정
-  const topicType = topic.split("/")[TOPIC_TYPE_INDEX];
-  const messageJson = JSON.parse(message);
+  const topicType = topic.split("/")[TOPIC_TYPE_INDEX]; // data, registration
+  const subTopic = topic.split("/")[SUB_TOPIC_INDEX]; // deviceId,
 
   try {
-    // message event 콜백 로직 추가
-    switch (topicType) {
-      case "dt":
-        db.insertData({
-          device_id: messageJson.device_id,
-          plantName: messageJson.plantName,
-          plantNickName: messageJson.plantNickName,
-          created_at: new Date(messageJson.timestamp),
-          memo: messageJson.memo,
-          potDiameter: messageJson.potDiameter,
-          potHeight: messageJson.potHeight,
-          plantDiameter: messageJson.plantDiameter,
-          plantHeight: messageJson.plantHeight,
-          region: messageJson.region,
-          temperature: messageJson.temperature,
-          humid: messageJson.humid,
-          soil: messageJson.soil,
+    if (topicType == "registration") {
+      topicDeviceId = subTopic;
+      console.log(subTopic);
+      console.log(message.toString());
+      const result = await db.insertDevice(subTopic);
+      console.log(result);
+    } else if (topicType == "data") {
+      if (subTopic == "B4:8A:0A:75:A4:40") {
+        const messageJson = JSON.parse(message);
+        // const data1 = messageJson.sensor_data.replace(/,/g, "");
+        console.log(messageJson.connection_device_id);
+        const data = messageJson.sensor_data.split(",");
+        const sensor_data = {};
+        for (var i = 0; i < data.length; i++) {
+          Object.assign(sensor_data, JSON.parse(data[i]));
+        }
+        console.log(sensor_data);
+        console.log("==========messageJson==================");
+        console.log(messageJson);
+        console.log("=======================================");
+        db.insertRealtimeData({
+          deviceId: messageJson.connection_device_id,
+          temperature: parseFloat(sensor_data.temper),
+          humid: parseFloat(sensor_data.humid),
+          soil: parseFloat(sensor_data.soil),
+          createdAt: new Date(),
         });
-        break;
-      default:
-        console.log("모르는 토픽");
-        break;
+      }
+    } else {
+      console.log("모르는 토픽");
     }
   } catch {
     console.log(error);
